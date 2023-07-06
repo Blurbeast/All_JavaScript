a75f9acd13943cf96d27831831fa4be6017dfdc4
Blurbeast <o.samuel@native.semicolon.africa>
2023-07-06 17:54:05 +0100

    running

---
+++ Arrays.js
@@ -0,0 +1,61 @@
+let arr = new Array(1,2,3,4,5,6,7)
+let arr1 = [0,4,5]
+// array here can take any dataType
+// the methods are push & pop, concat, shift & unshift
+console.log(arr.length)
+//the push method add to the last element in the array
+arr.push("Hello")
+// the pop removes the last element in the array
+arr.pop
+// shift remove the first element in the array
+arr1.shift()
+// unshift add the new element to the first place in the list i.e the index 0
+arr1.unshift("Get")
+// the concat join two variables together 
+let arr2 = arr.concat(arr1)
+
+console.log(arr2)
+
+// slice , the principle of slicing here is same as that of python, start stop and step
+let result = arr.slice(1)
+let result1 = arr.slice(1,3)
+console.log(result)
+console.log(result1)
+
+//splice can delete as well as add to the array again
+// splice is splice(2-> start from this index, 2 -> number of elements to be deleted, 100-> you might decide to add to the array)
+let spResult = arr.splice(1,2)
+console.log(arr)
+console.log(spResult)
+let spResult2 = arr.splice(1,2, "esther", "gift")
+console.log(spResult2)
+
+// global and prototype
+Array.prototype.lol = "hhhhhaaaaaaaaaaaaaa";
+Array.prototype.loml = "Temiiiii";
+
+console.log(arr.lol)
+console.log(arr.loml)
+
+Array.prototype.sum = function(){
+    let total =0;
+    for(let index = 0; index< this.length; index++){
+        total+= this[index];
+    }
+return total;
+}
+let newArray = [1,2,3,4]
+console.log(newArray.sum())
+let arrr = [4,7,10,10,2,10,9,100,100,99,99] 
+
+Array.prototype.maximum = function(){
+    let max = arrr[0]
+    let secondMax =0;
+    for(let index = 0; index < arrr.length; index++){
+        let number = arrr[index]
+        if(max < number) max = number;
+        if(max > number && number > secondMax) secondMax = number
+    }
+    return secondMax;
+}
+console.log("Max number",newArray.maximum())
---
+++ MultiArray.js
@@ -0,0 +1,43 @@
+// let arr = new Array()
+// let inArray = new Array()
+// let anotherArray = [["car", 2000], ["Truck", 500], ["Bike", 200]]
+// let car = ["car", 2000, ["Toyota", "Nissan"]]
+// let truck = ["Truck", 500, ["Ford"]]
+// let bike = ["Bike", 200, ["Honda"]]
+// let scooter = ["Scooter", 1500,null]
+
+// arr.push(anotherArray)
+// console.table(bike)
+
+// console.table(arr)
+// inArray.push(car)
+// inArray.push(truck)
+// inArray.push(bike)
+// inArray.push(scooter)
+// console.table(inArray)
+
+// console.log(inArray)
+
+let extraction = new Array()
+// let row1 = [200, 50, 300, 5]
+// let row2 = [10, 25, 7, 100]
+// let row3 = [25, 20, 70, 45]
+// let row4 = [500, 170, 11, 35]
+
+// extraction.push(row1)
+// extraction.push(row2)
+// extraction.push(row3)
+// extraction.push(row4)
+
+// console.table(extraction)
+
+// let prompt = require('prompt-sync')()
+
+// let rowRequest = Number(prompt("Enter row number: "))
+// if (rowRequest<0 || rowRequest > 3) console.log("Out of bound")
+// let startColumn = Number(prompt("Enter column start number: "))
+// if(startColumn < 0 || startColumn > row1.length - 1) console.log("start column out of bound")
+// let endColumn = Number(prompt("Enter end number: "))
+// if(endColumn < 0 || endColumn > row1.length - 1) console.log("start column out of bound")
+
+// console.log(extraction[rowRequest].slice(startColumn, endColumn))
